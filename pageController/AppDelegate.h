2e826edb2812e288797c34145ade076880803842
maomao <1431608414@qq.com>
2016-09-10 10:57:03 +0800

    Initial Commit

---
+++ pageController/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  pageController
+//
+//  Created by 毛毛 on 16/9/10.
+//  Copyright © 2016年 锐拓. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ pageController/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  pageController
+//
+//  Created by 毛毛 on 16/9/10.
+//  Copyright © 2016年 锐拓. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
